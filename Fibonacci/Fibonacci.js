bcc456a2443f8966aebd697f065d228e1f191094
Gabriel Kloppel <tkloppel@gmail.com>
2024-03-28 02:47:41 -0300

    Alteração no Fibonacci e README.md

--- Fibonacci/Fibonacci.js
+++ Fibonacci/Fibonacci.js
@@ -1,24 +1,21 @@
-class Fibonacci {
-    /**
-    2) Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o próximo valor sempre será a soma 
-    dos 2 valores anteriores (exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...), escreva um programa na 
-    linguagem que desejar onde, informado um número, ele calcule a sequência de Fibonacci e retorne
-    uma mensagem avisando se o número informado pertence ou não a sequência.
+let termo1 = 0;
+let termo2 = 1;
+let termo3 = 0;
 
-    IMPORTANTE:
-    Esse número pode ser informado através de qualquer entrada de sua preferência ou pode ser previamente definido no código;
-     */
+console.log('-'.repeat(42));
+console.log('   Consulta da Sequência de Fibonacci   ');
+console.log('-'.repeat(42));
 
-    static isFibonacci(number) {
-        let sequence = [0, 1];
-        if (number === 0 || number === 1) {
-            return true;
-        }
-        while (sequence[1] < number) {
-            let temp = sequence[0];
-            sequence[0] = sequence[1];
-            sequence[1] = sequence[1] + temp;
-        }
-        return sequence[1] === number;
-    }
+const valor = parseInt(prompt('Digite um número: '));
+
+while (valor > termo3) {
+    termo3 = termo1 + termo2;
+    termo1 = termo2;
+    termo2 = termo3;
+}
+
+if (valor === 0 || valor === 1 || valor === termo3) {
+    console.log('O número faz parte da sequência de Fibonacci.');
+} else {
+    console.log('O número digitado não faz parte da sequência de Fibonacci.');
 }
--- README.md
+++ README.md
@@ -41,7 +41,7 @@ d) 4, 16, 36, 64,`100`
 
 e) 1, 1, 2, 3, 5, 8,`13`
 
-f) 2,10, 12, 16, 17, 18, 19,`20`
+f) 2,10, 12, 16, 17, 18, 19,`200`
 
 4. Você está em uma sala com três interruptores, cada um conectado a uma lâmpada em uma sala diferente. Você não pode ver as lâmpadas da sala em que está, mas pode ligar e desligar os interruptores quantas vezes quiser. Seu objetivo é descobrir qual interruptor controla qual lâmpada.
 
